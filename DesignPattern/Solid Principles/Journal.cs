434d1dd81c380a3df8ece178c6d87e7b7c3f37f2
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-03-01 23:38:49 +0200

    Add project files.

---
+++ DesignPattern/Solid Principles/Journal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Solid_Principles;
+
+public class Journal
+{
+	private readonly List<string> entriesList = new();
+	private static int count = 0;
+
+	public int AddEntry(string text)
+	{
+		entriesList.Add($"{++count} : {text}");
+		return count;
+	}
+
+	public void RemoveEntry(int index)
+	{
+		entriesList.RemoveAt(index);
+	}
+
+	public void Save(string filename)
+	{
+		// Save Entires To File
+	}
+
+	public static Journal Load(string filename)
+	{
+		// Load Journal From The File Name
+		return null;
+	}
+
+	public void Load(Uri uri)
+	{
+		// Load Journal From URI
+	}
+
+
+
+}
