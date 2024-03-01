458bae6ef8c1726efc960afc1ddcd03255493370
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-03-02 00:20:29 +0200

    .

---
+++ DesignPattern/Solid Principles/Single Responsibility/Journal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Solid_Principles;
+
+public class Journal
+{
+	private readonly List<string> entriesList = new();
+	private static int count = 0;
+
+	public int AddEntry(string text)
+	{
+		entriesList.Add($"{++count} : {text}");
+		return count;
+	}
+
+	public void RemoveEntry(int index)
+	{
+		entriesList.RemoveAt(index);
+	}
+
+}
+
---
+++ DesignPattern/Solid Principles/Single Responsibility/Persistance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Solid_Principles
+{
+	public class Persistance
+	{
+		public void SaveToFile(Journal j, string filename)
+		{
+			// Save Entires of particual Journal To File
+		}
+
+		public static Journal Load(string filename)
+		{
+			// Load Journal From The File Name
+			return null;
+		}
+
+		public void Load(Uri uri)
+		{
+			// Load Journal From URI
+		}
+	}
+}
