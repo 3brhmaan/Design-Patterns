b568d13b7307f348fdf346a63fa5abbb05f09ad9
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-03-02 03:08:37 +0200

    violating the dependency inversion principle by making the high level class "Research" depend on low level class "Relationship" i will not be able to change the low level code without breaking the entire code , suppose in the low level i want to change a method or a property that in the high level it depend on it then i will break the code

---
+++ DesignPattern/Solid Principles/Dependency Inversion/Example.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Solid_Principles.Dependency_Inversion
+{
+	public enum Relationship
+	{
+		parent , 
+		child , 
+		sibling
+	}
+
+	public class Person
+	{
+		public string name;
+	}
+	// Low-Level
+	public class Relationships
+	{
+		private List<(Person, Relationship, Person)> relations = new();
+
+		public void AddParentAndChild(Person parent, Person child)
+		{
+			relations.Add((parent , Relationship.parent , child));
+			relations.Add((child , Relationship.child , parent));
+		}
+
+		public List<(Person, Relationship, Person)> Relations => relations;
+	}
+	// High Level
+	public class Research
+	{
+		public Research(Relationships relationships)
+		{
+			var relations = relationships.Relations;
+
+			var res = relations.Where(
+				r => r.Item1.name == "John" &&
+				     r.Item2 == Relationship.parent);
+
+			foreach (var relation in res)
+			{
+				//
+			}
+		}
+
+	}
+}
