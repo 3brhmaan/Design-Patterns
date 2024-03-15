172a7f922a6e139ec98a6d58e86c251dd0e9d60c
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-03-15 03:08:33 +0200

    life without the builder pattern simple example to create the <p> HTML element

---
+++ DesignPattern/Builder Pattern/HTMLBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Builder_Pattern
+{
+    public class HTMLBuilder
+    {
+        StringBuilder builder = new StringBuilder();
+
+        public void Append(string text)
+        {
+            builder.Append(text);
+        }
+
+        public override string ToString()
+        {
+            return builder.ToString();
+        }
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DesignPattern.Solid_Principles.Open_Closed_Principle;
 using System;
+using System.Text;
+using DesignPattern.Builder_Pattern;
 using DesignPattern.Solid_Principles.Liskov_substitution;
 
 namespace DesignPattern
@@ -7,10 +9,16 @@ namespace DesignPattern
 	internal class Program
 	{ 
 		static void Main(string[] args)
-		{
-			
+        {
+            // <p>hello</p>
+            var hello = "hello";
+            HTMLBuilder builder = new HTMLBuilder();
+            builder.Append("<p>");
+            builder.Append(hello);
+            builder.Append("</p>");
 
 
-		}
+
+        }
 	}
 }
