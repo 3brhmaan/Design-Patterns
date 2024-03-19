3470493a01574b75cd2eebc93da84819a7ef61bf
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-03-20 01:54:59 +0200

    perototype inheritance version 2 solve the problem of the 1st version which are : 1. fully initialized constructor 2. repeatitive code

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using DesignPattern.Prototype_Pattern.PrototypeInheritanceV1;
+﻿using DesignPattern.Prototype_Pattern.PrototypeInheritanceV2;
+using System;
 
 namespace DesignPattern;
 
@@ -9,13 +9,19 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var john = new Employee(new string[] { "john", "switch" } ,
-            new Address("Meet Hadid", 123) ,3000);
+        var john = new Employee();
+        john.Names = new string[] { "john", "switch" };
+        john.Address = new Address("Meet Hadid", 123);
+        john.Salary = 3000;
 
         var copy = john.DeepCopy();
         copy.Names[0] = "ahmed";
 
         Console.WriteLine(john);
         Console.WriteLine(copy);
+
+
+        var e = john.DeepCopy();
+        var p = john.DeepCopy<Person>();
     }
 }
---
+++ DesignPattern/Prototype Pattern/PrototypeInheritanceV2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Prototype_Pattern.PrototypeInheritanceV2
+{
+    public interface IDeepCopyable<T> where T : new()
+    {
+        void CopyTo(T target);
+
+        public T DeepCopy()
+        {
+            var t = new T();
+            CopyTo(t);
+            return t;
+        }
+    }
+
+    public class Address : IDeepCopyable<Address>
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address()
+        {
+        }
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}," +
+                   $"{nameof(HouseNumber)}: {HouseNumber}";
+        }
+
+        public void CopyTo(Address target)
+        {
+            target.StreetName = StreetName;
+            target.HouseNumber = HouseNumber;
+        }
+    }
+
+    public class Person : IDeepCopyable<Person>
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person()
+        {
+        }
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"Names : {string.Join(" ", Names)}, " +
+                   $"Address : {Address}";
+        }
+
+        public void CopyTo(Person target)
+        {
+            target.Names = (string[])Names.Clone();
+            target.Address = Address.DeepCopy();
+        }
+    }
+
+    public class Employee : Person, IDeepCopyable<Employee>
+    {
+        public int Salary;
+
+        public Employee()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, {nameof(Salary)}: {Salary}";
+        }
+
+        public void CopyTo(Employee target)
+        {
+            base.CopyTo(target);
+            target.Salary = Salary;
+        }
+    }
+
+    public static class ExtensionMethod
+    {
+        public static T DeepCopy<T>(this IDeepCopyable<T> item)
+            where T : new()
+        {
+            return item.DeepCopy();
+        }
+
+        public static T DeepCopy<T>(this T person)
+            where T : Person, new()
+        {
+            return ((IDeepCopyable<T>)person).DeepCopy();
+        }
+    }
+}
