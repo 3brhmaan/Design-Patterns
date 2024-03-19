1fcf10884da5cc8fa0ab575badfe2864334b2e34
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-03-19 04:14:25 +0200

    ICloneable Approach this approach is based on IClonable Interface and it's BAD because : 1. the clone method return object not the actual type 2. there is no grante that Clone method make Deep Copy so it's dangrouse to use it

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using DesignPattern.Factories_Pattern.FactoryMethod;
+using DesignPattern.Prototype_Pattern.ICloneableApproach;
 
 namespace DesignPattern;
+
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        var john = new Person(new[] { "john", "switch" },
+            new Address("Meet Hadid", 123));
+
+        var jane = john.Clone() as Person;
+        jane.Names[0] = "Jane";
+
+        Console.WriteLine(john);
+        Console.WriteLine(jane);
     }
 }
---
+++ DesignPattern/Prototype Pattern/ICloneableApproach.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Prototype_Pattern.ICloneableApproach;
+
+public class Person : ICloneable
+{
+    public string[] Names;
+    public Address Address;
+
+    public Person(string[] names, Address address)
+    {
+        Names = names;
+        Address = address;
+    }
+
+    public override string ToString()
+    {
+        return $"Names : {string.Join(" ", Names)}, Address : {Address}";
+    }
+
+    public object Clone()
+    {
+        return new Person(Names, Address);
+    }
+}
+
+public class Address
+{
+    public string StreetName;
+    public int HouseNumber;
+
+    public Address(string streetName, int houseNumber)
+    {
+        StreetName = streetName;
+        HouseNumber = houseNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+    }
+}
