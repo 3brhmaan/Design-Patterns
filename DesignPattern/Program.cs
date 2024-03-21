a87ae842dd7bc131d387f2df8ecfcf4b9d0b65e9
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-03-21 04:39:45 +0200

    singletone design pattern implement singleton design pattern

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,5 +1,5 @@
-﻿using DesignPattern.Prototype_Pattern.PrototypeInheritanceV2;
-using System;
+﻿using System;
+using DesignPattern.Singleton_Pattern.SingletonPattern;
 
 namespace DesignPattern;
 
@@ -9,19 +9,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var john = new Employee();
-        john.Names = new string[] { "john", "switch" };
-        john.Address = new Address("Meet Hadid", 123);
-        john.Salary = 3000;
-
-        var copy = john.DeepCopy();
-        copy.Names[0] = "ahmed";
-
-        Console.WriteLine(john);
-        Console.WriteLine(copy);
-
-
-        var e = john.DeepCopy();
-        var p = john.DeepCopy<Person>();
+        var db = SingletonDatabase.Instance;
     }
 }
---
+++ DesignPattern/Singleton Pattern/SingletonPattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+
+namespace DesignPattern.Singleton_Pattern.SingletonPattern
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static SingletonDatabase instance = new();
+        public static SingletonDatabase Instance => instance;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+
+            capitals = File.ReadAllLines("capitals.txt")
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            if (capitals.ContainsKey(name))
+                return capitals[name];
+            else
+                return -1;
+        }
+    }
+}
