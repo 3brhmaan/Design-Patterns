905f9b5a2a5b9ebec60cdffcda2c0fd8d27cc191
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-03-23 03:57:54 +0200

    Monostate Pattern implement the monostate pattern , that allow users to share the same state with different object refrence

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using DesignPattern.Singleton_Pattern.SingletonPatternWithLazyLoading;
+using DesignPattern.Singleton_Pattern.MonostatePattern;
+
 
 namespace DesignPattern;
-
-
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var db = SingletonDatabase.Instance;
+        var ceo1 = new CEO();
+        ceo1.Name = "Adam";
+        ceo1.Age = 25;
+
+        var ceo2 = new CEO();
+        Console.WriteLine(ceo2);
     }
 }
---
+++ DesignPattern/Singleton Pattern/MonostatePattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton_Pattern.MonostatePattern
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Age)}: {Age}, {nameof(Name)}: {Name}";
+        }
+    }
+}
