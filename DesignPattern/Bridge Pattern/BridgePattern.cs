eee65d4e520c8c49cb342fb784e2eae9ccef340c
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-04-15 01:19:53 +0200

    implement Bridge pattern implementing the bridge pattern and decopule interface from implementation and avoid cartesian product scenario

---
+++ DesignPattern/Bridge Pattern/BridgePattern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Bridge_Pattern
+{
+    public interface IRender
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRender : IRender
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Drawing a Circle of Radius : {radius}");
+        }
+    }
+
+    public class RasterRender : IRender
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Drawing Pixeles for Circle with Radius : {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRender render;
+        protected Shape(IRender render)
+        {
+            this.render = render;
+        }
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        float radius;
+        public Circle(IRender render, float radius) : base(render)
+        {
+            this.radius = radius;
+        }
+        public override void Draw()
+        {
+            render.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using DesignPattern.Adapter_Pattern.VectorRaster;
-using static DesignPattern.Adapter_Pattern.GenericValueAdapter;
+﻿using DesignPattern.Bridge_Pattern;
+using System;
 
 
 namespace DesignPattern;
@@ -10,15 +9,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var v = new Vector2i(1, 2);
-        var vv = new Vector2i(3, 2);
-
-        var res = v + vv;
-
-        var tmp = Vector3f.Create(1.5f, 5.3f, 2.4f);
-        Console.WriteLine(tmp);
+        var circle = new Circle(new RasterRender() , 5f);
+        circle.Draw();
     }
 }
-
-
-
