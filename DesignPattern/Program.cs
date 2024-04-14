754e3bfec09da2bbdb291f01e1ec12a4e8cd3376
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-04-14 20:45:58 +0200

    impelemt generic value adapter impleneting the generic value adapter throught adapt a value to a type using interface/abstract class to yield the value

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -5,36 +5,12 @@ using DesignPattern.Adapter_Pattern.VectorRaster;
 namespace DesignPattern;
 
 
-
 internal class Program
 {
-    private static readonly List<VectorObject> vectorObjects
-        = new()
-        {
-            new VectorRectangle(1, 1, 10, 10),
-            new VectorRectangle(3, 3, 6, 6)
-        };
-
-    public static void DrawPoint(Point p)
-    {
-        Console.Write(".");
-    }
-
-    private static void Draw()
-    {
-        foreach (var vo in vectorObjects)
-        {
-            foreach (var line in vo)
-            {
-                var pointsAdapter = new LineToPointAdapter(line);
-                foreach (var point in pointsAdapter) 
-                    DrawPoint(point);
-            }
-        }
-    }
-
     private static void Main(string[] args)
     {
-        Draw();
     }
 }
+
+
+
